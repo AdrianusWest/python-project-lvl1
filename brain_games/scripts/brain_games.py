85015e93d80e056747388c5a406b65b6ab76af20
Adrianus West <adrianus_west@protonmail.com>
2021-06-15 23:23:27 +0300

    Step 3

---
+++ brain_games/scripts/brain_games.py
@@ -0,0 +1,13 @@
+from brain_games.cli import welcome_user
+
+"""Приветствие"""
+
+
+def main():
+    print('Welcome to the Brain Games!')
+    welcome = welcome_user()
+    print('Hello,' + welcome + '!')
+
+
+if __name__ == '__main__':
+    main()
