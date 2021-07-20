38f7d357e1f6ab45e2cfa6c1664b5a4b63f2bf16
Adrianus West <adrianus_west@protonmail.com>
2021-07-20 13:08:44 +0300

    Edits_2 corr.

--- brain_games/games/brain_progression.py
+++ brain_games/games/brain_progression.py
@@ -8,7 +8,10 @@ def get_question_and_answer():
     start_number = random.randint(1, 1000)
     progression_length_count = 11
 
-    progression = list(range(start_number, (start_number + progression_length_count * step), step))
+    progression = list(range
+                       (start_number,
+                        (start_number + progression_length_count * step),
+                        step))
 
     random_index = random.randrange(0, progression_length_count)
     answer, progression[random_index] = progression[random_index], '..'
