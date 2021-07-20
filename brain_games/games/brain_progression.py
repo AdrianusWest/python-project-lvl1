d8f799c08619c1bad3f42d961bf90a393968cc40
Adrianus West <adrianus_west@protonmail.com>
2021-07-20 12:50:57 +0300

    Edits_2 corr.

--- brain_games/games/brain_progression.py
+++ brain_games/games/brain_progression.py
@@ -5,20 +5,12 @@ INSTRUCTION = 'What number is missing in the progression?'
 
 def get_question_and_answer():
     step = random.randint(1, 10)
-    random_number = random.randint(1, 1000)
-    counter = 0
-    progression = ''
-    random_step = random.randint(0, 9)
+    start_number = random.randint(1, 1000)
     progression_length_count = 11
 
-    while counter <= progression_length_count:
-        if counter == random_step:
-            chunk = '..'
-            correct_answer = str(random_number)
-        else:
-            chunk = str(random_number)
-        progression += (' ' + chunk)
-        random_number += step
-        counter = counter + 1
-    question = f'{progression.lstrip()}'
-    return question, correct_answer
+    progression = list(range(start_number, (start_number + progression_length_count * step), step))
+
+    random_index = random.randrange(0, progression_length_count)
+    answer, progression[random_index] = progression[random_index], '..'
+    question = ' '.join(map(str, progression))
+    return question, str(answer)
